afe2ea44f37e2f33592aacd4a849cd0bfeb6ed24
giparipradinaa <giparipradinaabdillah@gmail.com>
2022-11-20 11:12:50 +0700

    first commit

---
+++ connection.php
@@ -0,0 +1,15 @@
+<?php   
+
+$databaseHost = 'localhost';
+$databaseName = 'uts';
+$databaseUsername = 'root';
+$databasePassword = '';
+
+$mysqli = mysqli_connect(
+    $databaseHost,
+    $databaseUsername,
+    $databasePassword,
+    $databaseName
+);
+
+?>
